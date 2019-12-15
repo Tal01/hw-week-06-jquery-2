a0f5c5d2205c933532d0835e440d18da251a7f80
Tal01 <56513447+Tal01@users.noreply.github.com>
2019-12-16 00:22:23 +0300

    Talah Adnan
    Last Version , Fixed the issues and solved

---
+++ main.js
@@ -0,0 +1,20 @@
+// exercie 1
+$("button").eq(0).click(function(){
+    $("#secret").fadeToggle("fast");
+    
+    
+    })
+// exercise 2
+$("button").eq(1).click(function(){
+
+    $(".secret").slideUp("fast")
+
+
+})
+
+// //exercise 3
+$("button").eq(2).click(function(){
+
+$("img").attr("src", "img/highfive.gif");
+
+})
